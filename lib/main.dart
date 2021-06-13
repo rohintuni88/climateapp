0bfb62f21e61a25f155a7faa333f40db5ec899d6
rohintuni88 <rohintuni88@gmail.com>
2021-06-13 17:15:04 +0800

    version

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,6 @@ import 'Widget/WeatherItem.dart';
 import 'models/ForecastData.dart';
 import 'models/WeatherData.dart';
 
-
 void main() => runApp(new MyApp());
 
 class MyApp extends StatefulWidget {
@@ -45,49 +44,51 @@ class MyAppState extends State<MyApp> {
             title: Text('Flutter Weather App'),
           ),
           body: Center(
+              child: Column(mainAxisSize: MainAxisSize.min, children: <Widget>[
+            Expanded(
               child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: <Widget>[
-                    Expanded(
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: <Widget>[
-                          Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: weatherData != null ? Weather(weather: weatherData) : Container(),
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: weatherData != null
+                        ? Weather(weather: weatherData)
+                        : Container(),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: isLoading
+                        ? CircularProgressIndicator(
+                            strokeWidth: 2.0,
+                            valueColor:
+                                new AlwaysStoppedAnimation(Colors.white),
+                          )
+                        : IconButton(
+                            icon: new Icon(Icons.refresh),
+                            tooltip: 'Refresh',
+                            onPressed: loadWeather,
+                            color: Colors.white,
                           ),
-                          Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: isLoading ? CircularProgressIndicator(
-                              strokeWidth: 2.0,
-                              valueColor: new AlwaysStoppedAnimation(Colors.white),
-                            ) : IconButton(
-                              icon: new Icon(Icons.refresh),
-                              tooltip: 'Refresh',
-                              onPressed: loadWeather,
-                              color: Colors.white,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                    SafeArea(
-                      child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Container(
-                          height: 200.0,
-                          child: forecastData != null ? ListView.builder(
-                              itemCount: forecastData.list.length,
-                              scrollDirection: Axis.horizontal,
-                              itemBuilder: (context, index) => WeatherItem(weather: forecastData.list.elementAt(index))
-                          ) : Container(),
-                        ),
-                      ),
-                    )
-                  ]
-              )
-          )
-      ),
+                  ),
+                ],
+              ),
+            ),
+            SafeArea(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  height: 200.0,
+                  child: forecastData != null
+                      ? ListView.builder(
+                          itemCount: forecastData.list.length,
+                          scrollDirection: Axis.horizontal,
+                          itemBuilder: (context, index) => WeatherItem(
+                              weather: forecastData.list.elementAt(index)))
+                      : Container(),
+                ),
+              ),
+            )
+          ]))),
     );
   }
 
@@ -106,7 +107,8 @@ class MyAppState extends State<MyApp> {
       if (e.code == 'PERMISSION_DENIED') {
         error = 'Permission denied';
       } else if (e.code == 'PERMISSION_DENIED_NEVER_ASK') {
-        error = 'Permission denied - please ask the user to enable it from the app settings';
+        error =
+            'Permission denied - please ask the user to enable it from the app settings';
       }
 
       location = null;
@@ -117,19 +119,17 @@ class MyAppState extends State<MyApp> {
       final lon = location['longitude'];
 
       final weatherResponse = await http.get(
-          'https://api.openweathermap.org/data/2.5/weather?APPID=0721392c0ba0af8c410aa9394defa29e&lat=${lat
-              .toString()}&lon=${lon.toString()}');
+          'https://api.openweathermap.org/data/2.5/weather?APPID=0721392c0ba0af8c410aa9394defa29e&lat=${lat.toString()}&lon=${lon.toString()}');
       final forecastResponse = await http.get(
-          'https://api.openweathermap.org/data/2.5/forecast?APPID=0721392c0ba0af8c410aa9394defa29e&lat=${lat
-              .toString()}&lon=${lon.toString()}');
+          'https://api.openweathermap.org/data/2.5/forecast?APPID=0721392c0ba0af8c410aa9394defa29e&lat=${lat.toString()}&lon=${lon.toString()}');
 
       if (weatherResponse.statusCode == 200 &&
           forecastResponse.statusCode == 200) {
         return setState(() {
           weatherData =
-          new WeatherData.fromJson(jsonDecode(weatherResponse.body));
+              new WeatherData.fromJson(jsonDecode(weatherResponse.body));
           forecastData =
-          new ForecastData.fromJson(jsonDecode(forecastResponse.body));
+              new ForecastData.fromJson(jsonDecode(forecastResponse.body));
           isLoading = false;
         });
       }
